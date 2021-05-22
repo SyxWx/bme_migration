23c268bc909ce114335e6ac083a08e47a3348206
“bme_songyuxin” <bme_songyuxin>
2021-05-22 18:49:57 +0800

    优化中间库-设备维护

--- bme-migration/src/main/java/com/bme/syx/cloud/service/DeviceInfoService.java
+++ bme-migration/src/main/java/com/bme/syx/cloud/service/DeviceInfoService.java
@@ -1,4 +1,9 @@
 package com.bme.syx.cloud.service;
 
+
+import org.springframework.stereotype.Service;
+
+
+@Service
 public class DeviceInfoService {
 }
--- bme-migration/src/main/java/com/bme/syx/cloud/web/DeviceInfoController.java
+++ bme-migration/src/main/java/com/bme/syx/cloud/web/DeviceInfoController.java
@@ -1,4 +1,14 @@
 package com.bme.syx.cloud.web;
 
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+//排放源清单
+@RestController
+@RequestMapping(value = "device")
 public class DeviceInfoController {
+
+
+
 }
