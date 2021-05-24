c6b2ef46928dbbe1a3c905c0a2d4cedb6bda50ec
“bme_songyuxin” <bme_songyuxin>
2021-05-24 18:09:56 +0800

    生产与测试环境

--- bme-migration/src/main/java/com/bme/syx/cloud/web/SignalController.java
+++ bme-migration/src/main/java/com/bme/syx/cloud/web/SignalController.java
@@ -4,7 +4,10 @@ import com.bme.syx.cloud.service.SignalService;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @RequestMapping(value="signal")
@@ -22,6 +25,17 @@ public class SignalController {
             customerId = "9999";
         }
         String  result =  signalService.insertSignalInfo(customerId);
+        return result;
+    }
+
+    //http://localhost:8080/mig/signal/import?customerId=8
+    @RequestMapping(value="fileimport")
+    @ResponseBody
+    public String importFileSignal(String customerId, @RequestParam(value="file",required = false) MultipartFile file){
+        if(!StringUtils.isNotBlank(customerId)){
+            customerId = "9999";
+        }
+        String  result =  signalService.insertFileSignalInfo(customerId,file);
         return result;
     }
 }
